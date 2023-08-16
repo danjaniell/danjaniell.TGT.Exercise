e337b3940e97045a1e2cd12a504176be9c215025
Jai Paragas <jparagas@flnt.io>
2023-08-16 17:47:11 +0800

    Refactor: Move test methods to service

--- TGT.Exercise.Service/TextService.cs
+++ TGT.Exercise.Service/TextService.cs
@@ -2,6 +2,45 @@
 {
     public class TextService
     {
+        public static int CountNonWhitespaceCharacters(string input)
+        {
+            int count = 0;
 
+            foreach (char c in input)
+            {
+                if (!char.IsWhiteSpace(c))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        public static int CountWords(string input)
+        {
+            int count = 0;
+            bool inWord = false;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char c = input[i];
+
+                if (!char.IsWhiteSpace(c))
+                {
+                    if (!inWord)
+                    {
+                        count++;
+                        inWord = true;
+                    }
+                }
+                else
+                {
+                    inWord = false;
+                }
+            }
+
+            return count;
+        }
     }
 }
--- TGT.Exercise.Test/TextTest.cs
+++ TGT.Exercise.Test/TextTest.cs
@@ -1,3 +1,5 @@
+using TGT.Exercise.Service;
+
 namespace TGT.Exercise.Test
 {
     public class TextTest
@@ -7,24 +9,9 @@ namespace TGT.Exercise.Test
         {
             string input = "lorem ipsum dolor";
 
-            int charCount = CountNonWhitespaceCharacters(input);
+            int charCount = TextService.CountNonWhitespaceCharacters(input);
 
             Assert.Equal(15, charCount);
-        }
-
-        public int CountNonWhitespaceCharacters(string input)
-        {
-            int count = 0;
-
-            foreach (char c in input)
-            {
-                if (!char.IsWhiteSpace(c))
-                {
-                    count++;
-                }
-            }
-
-            return count;
         }
 
         [Fact]
@@ -32,37 +19,9 @@ namespace TGT.Exercise.Test
         {
             string input = "lorem ipsum dolor";
 
-            int wordCount = CountWords(input);
+            int wordCount = TextService.CountWords(input);
 
             Assert.Equal(3, wordCount);
         }
-
-        public int CountWords(string input)
-        {
-            int count = 0;
-            bool inWord = false;
-
-            for (int i = 0; i < input.Length; i++)
-            {
-                char c = input[i];
-
-                if (!char.IsWhiteSpace(c))
-                {
-                    if (!inWord)
-                    {
-                        count++;
-                        inWord = true;
-                    }
-                }
-                else
-                {
-                    inWord = false;
-                }
-            }
-
-            return count;
-        }
-
-
     }
 }
