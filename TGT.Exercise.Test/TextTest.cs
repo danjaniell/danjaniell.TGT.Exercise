3a90653eae962f524927cca1c37f8b506256ed6d
Jai Paragas <jparagas@flnt.io>
2023-08-16 17:45:11 +0800

    Add tests : Count total number of characters and words

--- TGT.Exercise.Test/TextTest.cs
+++ TGT.Exercise.Test/TextTest.cs
@@ -3,13 +3,66 @@ namespace TGT.Exercise.Test
     public class TextTest
     {
         [Fact]
-        public void GetTotalCharactersFromInput()
+        public void TotalCharacterCountFromInputIsCorrect()
         {
             string input = "lorem ipsum dolor";
 
-            int charCount = 15;
+            int charCount = CountNonWhitespaceCharacters(input);
 
             Assert.Equal(15, charCount);
         }
+
+        public int CountNonWhitespaceCharacters(string input)
+        {
+            int count = 0;
+
+            foreach (char c in input)
+            {
+                if (!char.IsWhiteSpace(c))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        [Fact]
+        public void TotalWordCountFromInputIsCorrect()
+        {
+            string input = "lorem ipsum dolor";
+
+            int wordCount = CountWords(input);
+
+            Assert.Equal(3, wordCount);
+        }
+
+        public int CountWords(string input)
+        {
+            int count = 0;
+            bool inWord = false;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char c = input[i];
+
+                if (!char.IsWhiteSpace(c))
+                {
+                    if (!inWord)
+                    {
+                        count++;
+                        inWord = true;
+                    }
+                }
+                else
+                {
+                    inWord = false;
+                }
+            }
+
+            return count;
+        }
+
+
     }
 }
