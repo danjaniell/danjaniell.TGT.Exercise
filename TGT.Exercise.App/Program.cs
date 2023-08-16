2d4ef2cfe8fba7f5fadf4158bc4141adc31c6085
Jai Paragas <jparagas@flnt.io>
2023-08-16 17:32:39 +0800

    Refactor: Move around projects and folders

--- TGT.Exercise.App/Program.cs
+++ TGT.Exercise.App/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TGT.Exercise.Service;
+
+namespace TGT.Exercise.App
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int i = 1;
+            IAsyncEnumerable<string> streamData = ReadStreamService.Read();
+            foreach (string data in streamData.ToEnumerable())
+            {
+                Console.WriteLine($"{i++} - {data}");
+            }
+        }
+    }
+}
